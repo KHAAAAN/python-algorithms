365faf2288543fcb77d163127a8bc7479ba7d40f
Jay <jayk1994@gmail.com>
2016-02-23 22:47:21 -0800

    MAO.py added

--- MAO/MAO.py
+++ MAO/MAO.py
@@ -1,4 +1,16 @@
-L = [1,2,3,4]
+L = [7,2,3,5,0] 
+
+
+#question, given a list- return a list with the elements updated
+#as the product of all the numbers to it's right and left (not including itself)
 
 def MAO(L):
-    L = 
+   
+    totalProduct = 1
+    for num in L:
+        totalProduct *= num 
+
+    return [totalProduct//num for num in L if num != 0] #handles 0 case
+
+if __name__ == '__main__':
+   print(MAO(L)) 
